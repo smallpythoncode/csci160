9c77766a76da9cf0d97bfbae4a8c2f10ab526232
small_python <smallpythoncode@gmail.com>
2022-02-05 12:30:07 -0600

    Updated lab03b docstring

--- program03/lab03b_kwj_csci16_spr22.py
+++ program03/lab03b_kwj_csci16_spr22.py
@@ -6,10 +6,12 @@ Lab 3 Part B
 
 Prompt the user for an x and y coordinate.
 Repeat the coordinates back to the user using standard point notation
-declare what Cartesian coordinate quadrant the point belongs to.
+and declare what Cartesian coordinate quadrant the point belongs to.
 
 It's not part of the assignment, but I am going to declare whether the
-point is at the origin or along an axis.
+point is at the origin or along an axis. I don't like the idea of
+prompting a user for information (inputting 0) then not doing anything
+with it.
 """
 
 while True:
