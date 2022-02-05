e7a1bb3e75c57ee31ca183d630e9fe6ffd0d8d32
smallpythoncode <smallpythoncode@gmail.com>
2022-02-04 21:45:16 -0600

    i think i made progress with lab03b (?)

--- program03/lab03b_kwj_csci16_spr22.py
+++ program03/lab03b_kwj_csci16_spr22.py
@@ -15,33 +15,45 @@ point is at the origin or along an axis.
 while True:
     try:
         x_coord = float(input("Enter a value for x: "))
-    except ValueError:
-        print("That is not a valid entry.")
-    else:
-        break
-while True:
-    try:
         y_coord = float(input("Enter a value for y: "))
     except ValueError:
         print("That is not a valid entry.")
     else:
         break
 
-if x_coord and y_coord == 0:
+
+if x_coord == 0 and y_coord == 0:
     print("(0, 0) is the origin.")
 elif x_coord == 0:
-    print(f"(0, {y_coord}) is along the y-axis.")
+    print(f"(0, {y_coord:.2f}) is along the y-axis.")
 elif y_coord == 0:
-    print(f"({x_coord}, 0) is along the x-axis.")
+    print(f"({x_coord:.2f}, 0) is along the x-axis.")
+
+
+
+
+
+
+
 elif (x_coord and y_coord) > 0:
     # elif x_coord > 0 and y_coord > 0:
-    print(f"({x_coord}, {y_coord}) is in quadrant I.")
-elif x_coord < 0 < y_coord:
-    print(f"({x_coord}, {y_coord}) is in quadrant II.")
-elif x_coord < 0 and y_coord == 0:
-    print(f"({x_coord}, {y_coord}) is in quadrant II.")
-else:
-    pass
+    print("makes sense, right????")
 
+
+
+
+
+
+
+else:
+    print("arbitrary")
+# elif x_coord < 0 < y_coord:
+#     print(f"({x_coord}, {y_coord}) is in quadrant II.")
+# elif x_coord < 0 and y_coord == 0:
+#     print(f"({x_coord}, {y_coord}) is in quadrant II.")
+# else:
+#     pass
+
+print(f"({x_coord}, {y_coord})")
 
 # All work and no play makes Jack a dull boy.
