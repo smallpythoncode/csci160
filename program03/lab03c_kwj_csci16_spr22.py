f4519c533a5030bb2b2c1f84bc8f9a03fced847b
smallpythoncode <smallpythoncode@gmail.com>
2022-02-05 21:26:31 -0600

    Completed lab03c

--- program03/lab03c_kwj_csci16_spr22.py
+++ program03/lab03c_kwj_csci16_spr22.py
@@ -13,19 +13,32 @@ In order, prompt the user for:
 Print the result of the arithmetic.
 
 Error handling:
-    1. If the user selects an inappropriate operator (floor division,
-    modulo operation, exponentiation, or input is not related to
-    arithmetic), state that in input is invalid.
+    1. If the user selects an inappropriate operator (i.e., not
+    specified in Prompt 2), state that in input is invalid.
     2. If the user attempts to divide by zero, state that such an
     operation is not possible.
     3. No other error checking
 """
 
 print("Let's do some math.")
-first_num = float("Enter a number, any number: ")
-print('To add, enter "+".\nTo subtract, enter"-".\nTo multiply, enter "*".\n'
-      'To divide, enter"/".')
+first_num = float(input("Enter a number, any number: "))
+# print('To add, enter "+".\nTo subtract, enter"-".\nTo multiply, enter "*".\n'
+#       'To divide, enter"/".')
 operator = input("Enter an operator: ")
-
+if operator not in ["+", "-", "*", "/"]:
+    print("Invalid operator")
+else:
+    second_num = float(input("Enter another number, any number: "))
+    if second_num == 0 and operator == "/":
+        print("One does not simply divide by zero.")
+    else:
+        if operator == "+":
+            print("The result is", first_num + second_num)
+        if operator == "-":
+            print("The result is", first_num - second_num)
+        if operator == "*":
+            print("The result is", first_num * second_num)
+        if operator == "/":
+            print("The result is", first_num / second_num)
 
 # All work and no play makes Jack a dull boy.
