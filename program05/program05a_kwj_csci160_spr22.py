fed52f5221656c33d3c5eff57307b34115149cb5
smallpythoncode <smallpythoncode@gmail.com>
2022-02-16 22:30:53 -0600

    continued work on program05a

--- program05/program05a_kwj_csci160_spr22.py
+++ program05/program05a_kwj_csci160_spr22.py
@@ -26,20 +26,53 @@ Assumptions:
 - All data entered will be valid (i.e., no error checking is required).
 - No letter grade other than A, B, C, D, or F will be entered (upper
   case).
-- If no GPA can be calculated (i.e, num_attempted_credits == 0), do not
-  generate any output, simply end the program.
+- If no GPA can be calculated (e.g., ZeroDivisionError), do not generate
+  any output, simply end the program.
 """
 
-num_classes = int(input("Enter the number of classes this semeter: "))
-times_class_info_entered = 0
-attempted_credits = 0
+total_credits_attempted = 0
+credits_passed = 0
+classes_attempted = int(input("Enter the number of classes this semester: "))
+classes_passed = 0
+honor_points = 0
 
-if num_classes != 0:
-    while times_class_info_entered < num_classes:
+# if classes_attempted < 1:
+#     print("Then why are you even running this program?")
+# per third assumption
+if classes_attempted > 0:
+    times_class_info_entered = 0
+    while times_class_info_entered < classes_attempted:
         class_name = input("Enter course ID of class number "
-                           f"{times_class_info_entered + 1}: ")
-        attempted_credits += int(input(f"Enter number of credits {class_name} "
-                                       "is worth: "))
-        print(attempted_credits)
+                           f"{times_class_info_entered + 1}: ").upper()
+        class_credits_attempted = int(input("Enter number of credits"
+                                            f" {class_name} is worth: "))
+        total_credits_attempted += class_credits_attempted
+        class_letter_grade = input(f"Enter grade received for {class_name}: "
+                                   ).upper()
+        if class_letter_grade in ["A", "B", "C", "D"]:
+            classes_passed += 1
+            credits_passed += class_credits_attempted
+        if class_letter_grade == "A":
+            honor_points += 4 * class_credits_attempted
+        if class_letter_grade == "B":
+            honor_points += 3 * class_credits_attempted
+        if class_letter_grade == "C":
+            honor_points += 2 * class_credits_attempted
+        if class_letter_grade == "D":
+            honor_points += class_credits_attempted
+        times_class_info_entered += 1
+        print()
+
+# per third assumption
+try:
+    print(format("GPA:", "18s"),
+          f"{honor_points / total_credits_attempted:.4f}")
+    print("Credits attempted:", f"{total_credits_attempted:6d}")
+    print(format("Credits passed", "18s"), f"{credits_passed:6d}")
+    print("Classes attempted:", f"{classes_attempted:6d}")
+    print(format("Classes passed", "18s"), f"{classes_passed:6d}")
+except ZeroDivisionError:
+    pass
+# address assumption 3 with while loop avoiding ZeroDivisionError
 
 # All work and no play makes Jack a dull boy.
