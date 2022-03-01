0c667a7e036002b45331e9ba7b229d88bfb22d87
small_python <smallpythoncode@gmail.com>
2022-02-28 22:27:36 -0600

    zybooks_progress.py will track progress of...well...you know

---
+++ zybooks/zybooks_progress.py
@@ -0,0 +1,14 @@
+chapters = [
+    {"chapter": "1", "sections_assigned": 11, "sections_complete": 11},
+    {"chapter": "2", "sections_assigned": 11, "sections_complete": 11},
+    {"chapter": "3", "sections_assigned": 11, "sections_complete": 11},
+    {"chapter": "4", "sections_assigned": 13, "sections_complete": 13},
+    {"chapter": "5", "sections_assigned": 13, "sections_complete": 13},
+    {"chapter": "6", "sections_assigned": 17, "sections_complete": 17},
+    {"chapter": "7", "sections_assigned": 4, "sections_complete": 4},
+    {"chapter": "8", "sections_assigned": 18, "sections_complete": 3},
+    {"chapter": "12", "sections_assigned": 7, "sections_complete": 0}
+]
+
+total_sections = 
+print(total_sections)
