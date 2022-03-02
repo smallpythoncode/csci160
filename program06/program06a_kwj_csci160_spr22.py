4f1cb29f35eee2f1dff585610acba18c251f481f
smallpythoncode <smallpythoncode@gmail.com>
2022-03-01 20:10:02 -0600

    added file templates from program06a and program06b

---
+++ program06/program06a_kwj_csci160_spr22.py
@@ -0,0 +1,11 @@
+"""
+Jahnke
+kenny.jahnke@ndus.edu / greensaber77@gmail.com
+CSCI 160, Spring 2022, Lecture Sect 02, Lab Sect L03
+Program 06 Part A
+
+
+
+"""
+
+# All work and no play makes Jack a dull boy.
---
+++ program06/program06b_kwj_csci160_spr22.py
@@ -0,0 +1,13 @@
+"""
+Jahnke
+kenny.jahnke@ndus.edu / greensaber77@gmail.com
+CSCI 160, Spring 2022, Lecture Sect 02, Lab Sect L03
+Program 06 Part B
+
+
+
+"""
+
+from simple_graphics import SimpleGraphics
+
+# All work and no play makes Jack a dull boy.
