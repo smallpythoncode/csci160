28bb80465aa2cc70bd9d08623fae474997cbb215
smallpythoncode <smallpythoncode@gmail.com>
2022-03-08 19:02:39 -0600

    laid groundwork for program07

---
+++ program07/program07_kwj_csci160_spr22.py
@@ -0,0 +1,45 @@
+"""Jahnke, Student ID: 0808831
+kenny.jahnke@ndus.edu / greensaber77@gmail.com
+CSCI 160, Spring 2022, Lecture Sect 02, Lab Sect L03
+Program 07
+
+# TODO: Write premise of assignment
+
+Functions:
+
+    transaction_type() # TODO: list parameters
+        # TODO: summary
+    _read_data() # TODO: list parameters
+        # TODO: summary
+    # TODO: List functions
+
+Developer Note:
+
+    csv module may simplify implementation by virtue of delimiters
+    rather than using index() method
+
+"""
+
+
+def _read_data():
+    """ # TODO: explain purpose of reading test data file
+    # checking_account_data.txt
+
+    :return:
+    """
+
+    pass
+
+
+def transaction_type():
+    pass
+
+
+def main():
+    transaction_type()
+
+
+if __name__ == "__main__":
+    main()
+
+# All work and no play makes Jack a dull boy.
