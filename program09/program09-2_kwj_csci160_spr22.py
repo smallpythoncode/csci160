def0a93b08803e84f5b30f1eedaab86dc95015c9
smallpythoncode <smallpythoncode@gmail.com>
2022-03-30 21:29:23 -0500

    1. Renamed program09a to program09-1 to fit naming convention implied in instructions.; 2. Created file program09-2.

---
+++ program09/program09-2_kwj_csci160_spr22.py
@@ -0,0 +1,16 @@
+"""Jahnke, Student ID: 0808831
+kenny.jahnke@ndus.edu / greensaber77@gmail.com
+CSCI 160, Spring 2022, Lecture Sect 02, Lab Sect L03
+Program XX, Part X
+
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+# All work and no play makes Jack a dull boy.
