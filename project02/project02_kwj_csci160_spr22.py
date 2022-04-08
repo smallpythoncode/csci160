3737756c30afea82d14dc334a547b38d09c47fe8
small_python <smallpythoncode@gmail.com>
2022-04-08 12:25:55 -0500

    established skeletion for function definitions and module dosctring of functions

--- project02/project02_kwj_csci160_spr22.py
+++ project02/project02_kwj_csci160_spr22.py
@@ -2,10 +2,96 @@
 kenny.jahnke@ndus.edu / greensaber77@gmail.com
 CSCI 160, Spring 2022, Lecture Sect 02, Lab Sect L03
 Project 02
+Copyright (C) 2022 Kenneth Jahnke
 
 # TODO project description
 
+Required Functions:
+
+    readTempData (fileName)
+        # TODO
+    dailyHigh (highTemps, date)
+        # TODO
+    dailyLow (lowTemps, date)
+        # TODO
+    biggestDailyDifference (highTemps, lowTemps)
+        # TODO
+    dayOfBiggestDailyDifference (highTemps, LowTemps)
+        # TODO
+    smallestDailyDifference (highTemps, lowTemps)
+        # TODO
+    dayOfSmallestDailyDifferece (highTemps, lowTemps)
+        # TODO
+    monthlyAverage (temps)
+        # TODO
+    biggestDifferenceBetweenDays (temps)
+        # TODO
+    printTemps (monthYearInfo, highTemps, lowTemps)
+        # TODO
+    main()
+        Test each of the required functions.
+
+Discretionary Functions:
+
+    dateValidation () # TODO parameters
+        # TODO
 """
+
+
+# TODO
+def readTempData (fileName):
+    pass
+
+
+# TODO
+def dailyHigh (highTemps, date):
+    pass
+
+
+# TODO
+def dailyLow (lowTemps, date):
+    pass
+
+
+# TODO
+def biggestDailyDifference (highTemps, lowTemps):
+    pass
+
+
+# TODO
+def dayOfBiggestDailyDifference (highTemps, LowTemps):
+    pass
+
+
+# TODO
+def smallestDailyDifference (highTemps, lowTemps):
+    pass
+
+
+# TODO
+def dayOfSmallestDailyDifferece (highTemps, lowTemps):
+    pass
+
+
+# TODO
+def monthlyAverage (temps):
+    pass
+
+
+# TODO
+def biggestDifferenceBetweenDays (temps):
+    pass
+
+
+# TODO
+def printTemps (monthYearInfo, highTemps, lowTemps):
+    pass
+
+
+# discretionary
+# TODO
+def dateValidation ():
+    pass
 
 
 def main():
