cd43fadc6d242fdba4e8cae5c3b0c35e907336bd
smallpythoncode <smallpythoncode@gmail.com>
2022-04-07 19:22:02 -0500

    added data file and .py file that utlizes data file

---
+++ project02/project02_kwj_csci160_spr22.py
@@ -0,0 +1,18 @@
+"""Jahnke, Student ID: 0808831
+kenny.jahnke@ndus.edu / greensaber77@gmail.com
+CSCI 160, Spring 2022, Lecture Sect 02, Lab Sect L03
+Project 02
+
+# TODO project description
+
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+# All work and no play makes Jack a dull boy.
