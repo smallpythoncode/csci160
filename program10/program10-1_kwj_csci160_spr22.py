da5c3e842fe5c554fddee96081dd5ccedad44e79
small_python <smallpythoncode@gmail.com>
2022-04-13 12:36:12 -0500

    Began work on program10-1

--- program10/program10-1_kwj_csci160_spr22.py
+++ program10/program10-1_kwj_csci160_spr22.py
@@ -1,11 +1,69 @@
 """Jahnke, Student ID: 0808831
 kenny.jahnke@ndus.edu / greensaber77@gmail.com
 CSCI 160, Spring 2022, Lecture Sect 02, Lab Sect L03
-Program XX, Part X
+Program 10, Part 1
 Copyright (C) 2022 Kenneth Jahnke
 
+Prompt the user for a series of class names and the number of credits
+for each class. The class name must be in "DEPT NUM" format, for
+example, "CSCI 160". Assign every valid entry to a dictionary with the
+class as a string and the number of credits as an integer. Continue to
+ask for classes until the user enters a blank value.
+
+Prompt the user for a file name. Write the key-value pairs to the file
+with one pair per line and a tab character ("\t") between the key and
+value.
+
+Functions:
+
+    # TODO
 """
 
+# TODO
+"""check that the string is in proper format"""
+def classStringValidator ():
+
+    pass
+
+
+# TODO
+def creditsValidator ():
+    """check that the value for credits can be converted
+    to an in and that the value is between 0 and 4 inclusive"""
+    """fdsaf
+    
+    :param:
+    
+    """
+    # TODO
+    try:
+        pass
+    # TODO for function that uses "if variable is not None"
+    except ValueError:
+        return None
+
+
+def TINKERINGclassStringValidator (className):
+    if len(className) != 8:
+        return None
+    else:
+        singleSpacePresent = className.split()
+        if len(singleSpacePresent[0]) and len(singleSpacePresent[3]):
+            pass
+
+
+    """convert first 3 chars in number to int
+    last 1 to 3 to "L" or "HON" """
+
+    """DEPARTMENT NAME either 2 (EE), 3 (FIN) or 4 (CSCI) characters."""
+
+
+    x = "fdsaf3da"
+
+
+
+# if numCredits not in range (0, 5):
+    # prompt "are you sure?"
 
 def main():
     pass
