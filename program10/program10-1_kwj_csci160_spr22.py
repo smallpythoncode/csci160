4867eda897010dba653caf777cb7de3a278d422a
small_python <smallpythoncode@gmail.com>
2022-04-14 12:27:17 -0500

    Progress on program10-1. Est. completion: 35%

--- program10/program10-1_kwj_csci160_spr22.py
+++ program10/program10-1_kwj_csci160_spr22.py
@@ -16,12 +16,25 @@ value.
 
 Functions:
 
+    classStringValidator (className)
+        - Checks if className is a valid class name.
     # TODO
 """
 
+from os.path import isfile
+
 
 def classStringValidator (className):
-    """# TODO
+    """Checks if className is a valid class name.
+
+    Format of string: DEPT NUM
+    Allows for department names of 2-4 characters. Examples:
+        - EE 101
+        - FIN 310
+        - CSCI 160
+    Allows for lab and honors classes. Examples
+        - BIOL 150L
+        - CSCI 493HON
 
     :param str className: The name of the class to be validated
     :except AttributeError: className must be string
@@ -74,6 +87,93 @@ def creditsValidator ():
         return None
 
 
+# TODO modify
+def promptTempsDataFileName ():
+    # TODO modify
+    """Prompts user for name of data file containing temperature data.
+
+    Must include file extension, e.g., .txt.
+    Data format contract:
+        - Line 1: "Month Year", e.g., "March 2022"
+        - Line 2 - Line n. 'Day,HighTemp,LowTemp', e.g., "10,69,30"
+
+    .. warning::
+        Excessive blank lines within (> 0) or at end of data (> 1) may
+        cause exceptions if data file is argument of certain functions.
+
+    :return: The temperature data file name if file exists
+    :rtype: str or None
+    """
+    while True:
+        fileName = input(
+            "Enter name of temperature data file (include extension): ")
+        if isfile(fileName):
+            return fileName
+        elif fileName == "":
+            break
+        else:
+            print(f"{fileName} does not exist.\n"
+                  "Try entry again or press 'Enter'.")
+
+
+# TODO modify
+def promptFileNameWrite ():
+    # TODO modify
+    """Prompts user for desired name of text file for use in writing modes.
+
+    Designed to use in conjunction with writing of files, thus, checks if
+    text file of same name already exists to circumvent undesired
+    overwriting.
+
+    :return: The name of a .txt file
+    :rtype: str
+    """
+    while True:
+        fileName = input("Enter desired text file name. Do not add a file "
+                         "extension.\nFile name: ")
+        fileName = fileName + ".txt"
+        if not isfile(fileName):
+            return fileName
+        else:
+            print(fileName, "already exists. Do wish to overwrite?")
+            while True:
+                overwritePrompt = input("y/n: ").lower()
+                if overwritePrompt in ["y", "yes"]:
+                    return fileName
+                elif overwritePrompt in ["n", "no"]:
+                    break
+
+
+
+
+# TODO modify
+def promptFileNameRead ():
+    # TODO modify
+    """Prompts user for desired name of text file for use in read mode.
+
+    Designed to use in conjunction with writing of files, thus, checks if
+    text file of same name already exists to circumvent undesired
+    overwriting.
+
+    :return: The name of a .txt file
+    :rtype: str
+    """
+    while True:
+        fileName = input("Enter desired text file name. Do not add a file "
+                         "extension.\nFile name: ")
+        fileName = fileName + ".txt"
+        if isfile(fileName):
+            return fileName
+        # FIXME for reading
+        else:
+            print(fileName, "already exists. Do wish to overwrite?")
+            while True:
+                overwritePrompt = input("y/n: ").lower()
+                if overwritePrompt in ["y", "yes"]:
+                    return fileName
+                elif overwritePrompt in ["n", "no"]:
+                    break
+
 
 
 
@@ -82,7 +182,7 @@ def creditsValidator ():
     # prompt "are you sure?"
 
 def main():
-    pass
+    print(promptFileName())
 
 
 if __name__ == "__main__":
