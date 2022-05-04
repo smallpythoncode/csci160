6da5ba0b1b22db73cb9c0722ede44a43bf96c482
smallpythoncode <smallpythoncode@gmail.com>
2022-05-03 23:19:30 -0500

    added bare file for project03

---
+++ class_notes/20220502_other_languages.py
@@ -0,0 +1,47 @@
+"""
+Quiz 6 will be on dictionaries
+
+OnlineGDB
+onlinegdb.com/online_c_compiler
+
+3 types of languages
+Imperative languages telling the computer how to do something
+95% of the work being done, including Python
+
+functional languages resonate w/ people w/ strong math background
+UND doesn't have a course on that
+
+technative languages
+what you want to have done as a focus rather than how you want to do it
+
+C made in early 70s
+
+50s
+FORTRAN
+LISP
+COBAL
+
+Computers were hard to access, corporations and universities
+C made by bell labs
+also made unix
+C came free on the computers at universities
+
+Forcing indentation is not very common in most prog langs
+used for readability
+
+int main()
+{
+    return 0;
+}
+have to has a return statement if int, won't compile otherwise
+int dictates what type of return
+
+have to have a function called main to run
+
+in c, everything is in a function, same w/ c++
+java calls them modules
+
+most other languages have lines ending in a semicolon
+
+void type
+"""
---
+++ project03_kwj_csci160_spr22.py
@@ -0,0 +1,18 @@
+"""Jahnke, Student ID: 0808831
+kenny.jahnke@ndus.edu / smallpythoncode@gmail.com
+CSCI 160, Spring 2022, Lecture Sect 02, Lab Sect L03
+Program XX, Part X
+Copyright (C) 2022 Kenneth Jahnke
+
+"""
+
+# DUE ON MONDAY, MAY 9
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+# All work and no play makes Jack a dull boy.
