b809c5e42728eeabbe50e9614ed9661ddcb7c179
small_python <smallpythoncode@gmail.com>
2022-05-06 14:31:06 -0500

    added skeleton to project03

---
+++ project03/project03_kwj_csci160_spr22.py
@@ -0,0 +1,104 @@
+"""Jahnke, Student ID: 0808831
+kenny.jahnke@ndus.edu / smallpythoncode@gmail.com
+CSCI 160, Spring 2022, Lecture Sect 02, Lab Sect L03
+Project 03
+Copyright (C) 2022 Kenneth Jahnke
+
+# FIXME due Monday, May 9
+
+# TODO
+Assignment:
+
+
+
+# TODO
+Required Functions:
+    readClassInfo(fileName)
+        - #TODO
+    writeClassInfo(fileName, classes)
+        - #TODO
+    addClass(classes, className, grade, credits)
+        - #TODO
+    attemptedCredits(classes)
+        - #TODO
+    passedCredits(classes)
+        - #TODO
+    printClasses(classes)
+        - #TODO
+    getGPA(classes)
+        - #TODO
+    updateGrade(classes, className, newGrade)
+        - #TODO
+    classStatus(classes)
+        - #TODO
+    main()
+        - Testing all of the required functions.
+
+# TODO
+Discretionary Functions:
+
+
+
+
+"""
+
+
+# TODO
+def readClassInfo(fileName):
+    pass
+
+
+# TODO
+def writeClassInfo(fileName, classes):
+    pass
+
+
+# TODO
+def addClass(classes, className, grade, credits):
+    pass
+
+
+# TODO
+def attemptedCredits(classes):
+    pass
+
+
+# TODO
+def passedCredits(classes):
+    pass
+
+
+# TODO
+def printClasses(classes):
+    pass
+
+
+# TODO
+def getGPA(classes):
+    pass
+
+
+# TODO
+def updateGrade(classes, className, newGrade):
+    pass
+
+
+# TODO
+def classStatus(classes):
+    pass
+
+
+# TODO
+def main():
+    """Testing all of the required functions.
+
+    :rtype: None
+    """
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+# All work and no play makes Jack a dull boy.
